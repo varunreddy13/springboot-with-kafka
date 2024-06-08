876f8014930263ee4821984385b7b835c68290e6
Varun Reddy Musku <varunreddymusku@gmail.com>
2024-06-07 19:44:57 -0500

    intial commit

---
+++ src/main/java/com/example/springboot_with_kafka/SpringbootWithKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot_with_kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWithKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootWithKafkaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springboot_with_kafka/SpringbootWithKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot_with_kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWithKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
